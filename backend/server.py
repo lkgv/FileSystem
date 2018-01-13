0f91824bff76eec3cfebc575b168a0153a29a2ad
lkgv <lkgv@foxmail.com>
2018-01-13 14:49:13 +0800

    Naive frontend

---
+++ backend/server.py
@@ -0,0 +1,31 @@
+def getServerList():
+    data = [
+        {
+            "id": "02",
+            "ip": "192.160.0.2",
+            "used": "",
+            "remain": "",
+            "children": [
+                {
+                    "id": "04",
+                    "ip": "192.160.0.4",
+                    "used": "2.0GB",
+                    "remain": "4.0GB",
+                },
+                {
+                    "id": "05",
+                    "ip": "192.160.0.5",
+                    "used": "2.0GB",
+                    "remain": "4.0GB",
+                },
+                {
+                    "id": "06",
+                    "ip": "192.160.0.6",
+                    "used": "2.0GB",
+                    "remain": "4.0GB",
+                }
+            ]
+        }
+    ]
+    time.sleep(1)
+    return data
